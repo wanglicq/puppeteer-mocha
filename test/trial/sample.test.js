3494e78c71b7e0f468c0018626ad89d34cac03d9
wangli <wangli_cq@hotmail.com>
2018-10-18 15:24:49 +0800

    [Li]make all passed.

--- test/trial/sample.test.js
+++ test/trial/sample.test.js
@@ -2,6 +2,6 @@ const expect = require('chai').expect;
 const addContext = require('mochawesome/addContext');
 
 it('should have context', function () {
-    expect(1+1).to.equal(1);
+    expect(1+1).to.equal(2);
     addContext(this, 'context1');
 });
